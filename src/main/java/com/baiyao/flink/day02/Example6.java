f37c06c3aa26160c7ab97d57fe8400c64c2cfa56
baiyao-jrd <baiyao.org@gmail.com>
2022-08-19 19:45:13 +0800

    flatmap匿名函数实现数据的过滤，复制

---
+++ src/main/java/com/baiyao/flink/day02/Example6.java
@@ -0,0 +1,41 @@
+package com.baiyao.flink.day02;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/*
+ * flatmap
+ *
+ * 使用匿名函数编写出错详解
+ *
+ * 实现功能：
+ *
+ * 数据源【white、black、gray】
+ * 将white原封不动输出、black复制一份、gray过滤掉
+ *
+ * */
+public class Example6 {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        env
+                .fromElements("white", "black", "gray")
+                //1. 输入输出均是字符串
+                .flatMap(new FlatMapFunction<String, String>() {
+                    @Override
+                    public void flatMap(String in, Collector<String> out) throws Exception {
+                        if (in.equals("white")) {
+                            out.collect(in);
+                        } else if (in.equals("black")) {
+                            out.collect(in);
+                            out.collect(in);
+                        }
+                    }
+                })
+                .print("匿名类");
+
+        env.execute();
+    }
+}
