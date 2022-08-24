992ccc104bef5ccafbb461fa2fa4d6a12a7ec25a
baiyao-jrd <baiyao.org@gmail.com>
2022-08-25 00:42:50 +0800

    使用匿名函数(lambda函数)实现 - 泛型擦除

--- src/test/java/org/baiyao/flink/day02/Example06.java
+++ src/test/java/org/baiyao/flink/day02/Example06.java
@@ -1,19 +1,21 @@
 package org.baiyao.flink.day02;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
+
 /*
-* 使用flatmap实现过滤与复制
-* */
+ * 使用flatmap实现过滤与复制
+ * */
 public class Example06 {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         env
-                .fromElements("Red","Yellow","Blue")
+                .fromElements("Red", "Yellow", "Blue")
                 .flatMap(new FlatMapFunction<String, String>() {
                     @Override
                     public void flatMap(String in, Collector<String> out) throws Exception {
@@ -28,14 +30,30 @@ public class Example06 {
                 .print("使用匿名类方式实现过滤与复制");
 
         env
-                .fromElements("Red","Yellow","Blue")
+                .fromElements("Red", "Yellow", "Blue")
                 .flatMap(new MyFlatMap())
                 .print("使用外部类方式实现顾虑与复制");
+
+        env
+                .fromElements("Red", "Yellow", "Blue")
+                .flatMap((String in, Collector<String> out) ->
+                        {
+                            if (in.equals("Red")) {
+                                out.collect(in);
+                            } else if (in.equals("Yellow")) {
+                                out.collect(in);
+                                out.collect(in);
+                            }
+                        }
+                )
+                //类型擦除
+                .returns(Types.STRING)
+                .print("使用匿名函数(lambda函数)实现");
 
         env.execute();
     }
 
-    public static class MyFlatMap implements FlatMapFunction<String,String> {
+    public static class MyFlatMap implements FlatMapFunction<String, String> {
 
         @Override
         public void flatMap(String in, Collector<String> out) throws Exception {
