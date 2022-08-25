9b28ecee1f8a8dedc6b526597cf1ff73376f9bc8
baiyao-jrd <baiyao.org@gmail.com>
2022-08-25 16:14:48 +0800

    计算key为2的数据要去的reduce的并行子任务的索引值

---
+++ src/test/java/org/baiyao/flink/day02/Example09.java
@@ -0,0 +1,16 @@
+package org.baiyao.flink.day02;
+
+import org.apache.flink.util.MathUtils;
+
+/*
+* 计算key为2的数据要去的reduce的并行子任务的索引值
+* */
+public class Example09 {
+    public static void main(String[] args) {
+        Integer key = 2;
+        int index = (MathUtils.murmurHash(key.hashCode()) % 128) * 4 / 128;
+        System.out.println(index);
+
+        //reduce算子的并行度为4，第三个并行子任务的索引是2，128是默认的最大并行度
+    }
+}
